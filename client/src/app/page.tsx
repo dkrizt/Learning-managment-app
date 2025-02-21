ceaaca2153af02e22022d99592d1748b87a37abd
dkrizt <donkrizt@gmail.com>
2025-02-21 08:27:56 +0100

    Setup server, connect to local dynamodb, add api routes and controllers

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,15 @@
+import Footer from '@/components/Footer';
 import NonDashboardNavbar from '@/components/NonDashboardNavbar';
+import Landing from './(nondashboard)/landing/page';
 
 export default function Home() {
   return (
     <div className="nondashboard-layout">
       <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
+      <Footer />
     </div>
   );
 }
